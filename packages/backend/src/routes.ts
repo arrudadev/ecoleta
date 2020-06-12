2a63980c6b15f75366ca575793db18ee3e32e4f0
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2020-06-12 00:33:52 -0300

    feat(backend): add route to list items

--- packages/backend/src/routes.ts
+++ packages/backend/src/routes.ts
@@ -1,7 +1,18 @@
 import express from 'express';
 
+import knex from './database/connection';
+
 const routes = express.Router();
 
-routes.get('/', (request, response) => response.json({ teste: 'teste' }));
+routes.get('/items', async (request, response) => {
+	const items = await knex('items').select('*');
+
+	const serializedItems = items.map((item) => ({
+		title: item.title,
+		image_url: `${process.env.SERVER_URL}/uploads/${item.image}`,
+	}));
+
+	return response.json(serializedItems);
+});
 
 export default routes;
