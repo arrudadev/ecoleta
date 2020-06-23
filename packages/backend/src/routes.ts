3300a5b9c8c7578d5627a72a7510444e1d529916
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2020-06-22 23:17:14 -0300

    feat(backend): add route to create points

--- packages/backend/src/routes.ts
+++ packages/backend/src/routes.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 import knex from './database/connection';
 
@@ -8,11 +8,44 @@ routes.get('/items', async (request, response) => {
 	const items = await knex('items').select('*');
 
 	const serializedItems = items.map((item) => ({
+		id: item.id,
 		title: item.title,
 		image_url: `${process.env.SERVER_URL}/uploads/${item.image}`,
 	}));
 
 	return response.json(serializedItems);
+});
+
+routes.post('/points', async (request: Request, response: Response) => {
+	try {
+		const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
+
+		await knex.transaction(async (trx) => {
+			const insertedIds = await trx('points').insert({
+				image: 'image-fake',
+				name,
+				email,
+				whatsapp,
+				longitude,
+				latitude,
+				city,
+				uf,
+			});
+
+			const point_id = insertedIds[0];
+
+			const pointItems = items.map((item_id: number) => ({
+				point_id,
+				item_id,
+			}));
+
+			await trx('point_items').insert(pointItems);
+		});
+
+		return response.json({ success: true });
+	} catch (err) {
+		return response.status(500).json({ error: err });
+	}
 });
 
 export default routes;
