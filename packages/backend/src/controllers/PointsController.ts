a93537552043162f0b7f291926b4f85b4f30e21a
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2020-06-23 23:47:34 -0300

    refactor(backend): add temp unsplash url image to point creation

--- packages/backend/src/controllers/PointsController.ts
+++ packages/backend/src/controllers/PointsController.ts
@@ -41,7 +41,7 @@ class PointsController {
 			const { name, email, whatsapp, latitude, longitude, city, uf, items } = request.body;
 
 			const point = {
-				image: 'image-fake',
+				image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
 				name,
 				email,
 				whatsapp,
