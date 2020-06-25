41b93006e80bbe931d03c6f6573b342652bdc763
Alexandre Monteiro <alexandre.monteiro.bec@gmail.com>
2020-06-25 18:29:07 -0300

    feat(frontend): add leaflet to render map and axios to requests

--- packages/frontend/src/pages/CreatePoint/index.tsx
+++ packages/frontend/src/pages/CreatePoint/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 import { Link } from 'react-router-dom';
 
 import './styles.css';
@@ -59,6 +60,14 @@ const CreatePoint = () => (
 					<h2>Endereço</h2>
 					<span>Selecione o endereço no mapa</span>
 				</legend>
+
+				<Map center={[-23.6450973, -46.7858642]} zoom={15}>
+					<TileLayer
+						attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+						url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					/>
+					<Marker position={[-23.6450973, -46.7858642]} />
+				</Map>
 
 				<div className="field-group">
 					<div className="field">
