32c2c065261707a8e81d3a31cb9e2b3cf16aee92
Alexandre <alexandre.monteiro.bec@gmail.com>
2020-12-14 23:34:41 -0300

    chore: add Home page

---
+++ packages/mobile/src/pages/Home/index.tsx
@@ -0,0 +1,72 @@
+
+import React, { useState } from 'react';
+import { View, ImageBackground, Text, Image, TextInput, KeyboardAvoidingView, Platform } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import { Feather as Icon } from '@expo/vector-icons';
+
+import HomeBackground from '../../assets/home-background.png';
+import Logo from '../../assets/logo.png';
+import styles from './styles';
+
+const Home: React.FC = () => {
+	const [uf, setUf] = useState('');
+	const [city, setCity] = useState('');
+
+	// eslint-disable-next-line
+	function handleNavigateToPoints() {}
+
+	return (
+		<KeyboardAvoidingView
+			style={{ flex: 1 }}
+			behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+		>
+			<ImageBackground
+				source={HomeBackground}
+				style={styles.container}
+				imageStyle={{ width: 274, height: 368 }}
+			>
+				<View style={styles.main}>
+					<Image source={Logo} />
+					<View>
+						<Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+						<Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+					</View>
+				</View>
+
+				<View style={styles.footer}>
+					<TextInput
+						style={styles.input}
+						placeholder="Digite a UF"
+						value={uf}
+						maxLength={2}
+						autoCapitalize="characters"
+						autoCorrect={false}
+						onChangeText={setUf}
+					/>
+
+					<TextInput
+						style={styles.input}
+						placeholder="Digite a cidade"
+						value={city}
+						autoCorrect={false}
+						onChangeText={setCity}
+					/>
+
+					<RectButton style={styles.button} onPress={handleNavigateToPoints}>
+						<View style={styles.buttonIcon}>
+							<Text>
+								<Icon name="arrow-right" color="#FFF" size={24} />
+							</Text>
+						</View>
+						<Text style={styles.buttonText}>
+                            Entrar
+						</Text>
+					</RectButton>
+				</View>
+			</ImageBackground>
+		</KeyboardAvoidingView>
+	);
+};
+
+export default Home;
