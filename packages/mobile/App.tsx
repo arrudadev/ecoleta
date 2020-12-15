675cc2c52515d82c049b20adbeb7af96abeff0c0
Alexandre <alexandre.monteiro.bec@gmail.com>
2020-12-15 00:02:05 -0300

    chore(mobile): adding page routes

--- packages/mobile/App.tsx
+++ packages/mobile/App.tsx
@@ -1,8 +1,27 @@
-
 import React from 'react';
+import { StatusBar } from 'react-native';
 
-import Home from './src/pages/Home';
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
+import AppLoading from 'expo-app-loading';
+
+import Routes from './src/routes';
 
 export default function App() {
-	return <Home />;
+	const [fontsLoaded] = useFonts({
+		Roboto_400Regular,
+		Roboto_500Medium,
+		Ubuntu_700Bold,
+	});
+
+	if (!fontsLoaded) {
+		return <AppLoading />;
+	}
+
+	return (
+		<>
+			<StatusBar barStyle="dark-content" backgroundColor="transparent" translucent />
+			<Routes />
+		</>
+	);
 }
