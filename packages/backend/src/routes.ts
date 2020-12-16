0a029c1bf3b1f33563a0d1d9185cba1f5c569371
Alexandre <alexandre.monteiro.bec@gmail.com>
2020-12-16 18:59:27 -0300

    chore(backend): adding celebrate to validate point creation

--- packages/backend/src/routes.ts
+++ packages/backend/src/routes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import { celebrate, Joi } from 'celebrate';
 import multer from 'multer';
 
 import multerConfig from './config/multer';
@@ -12,7 +13,22 @@ const upload = multer(multerConfig);
 
 routes.get('/items', ItemsController.index);
 
-routes.post('/points', upload.single('image'), PointsController.create);
+routes.post('/points',
+	upload.single('image'),
+	celebrate({
+		body: Joi.object().keys({
+			name: Joi.string().required(),
+			email: Joi.string().required().email(),
+			whatsapp: Joi.number().required(),
+			latitude: Joi.number().required(),
+			longitude: Joi.number().required(),
+			city: Joi.string().required(),
+			uf: Joi.string().required().max(2),
+			items: Joi.string().required(),
+		}),
+	}, { abortEarly: false }),
+	PointsController.create);
+
 routes.get('/points/:id', PointsController.show);
 routes.get('/points', PointsController.index);
 
--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import { errors } from 'celebrate';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
@@ -15,5 +16,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+
+app.use(errors());
 
 app.listen(process.env.SERVER_PORT);
